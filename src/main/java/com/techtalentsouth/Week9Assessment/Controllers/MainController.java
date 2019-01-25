87a8a50fe56ac161f25fbc374a5207dd41929350
monali mayaskar <snehanatu@yahoo.com>
2019-01-25 09:48:32 -0500

    Initail Commit

---
+++ src/main/java/com/techtalentsouth/Week9Assessment/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.techtalentsouth.Week9Assessment.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public ModelAndView indexMethod() {
+		ModelAndView mv = new ModelAndView("index");
+		return mv;
+	}
+
+}
---
+++ src/main/java/com/techtalentsouth/Week9Assessment/Week9AssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.Week9Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week9AssessmentApplication.class, args);
+	}
+
+}
+
